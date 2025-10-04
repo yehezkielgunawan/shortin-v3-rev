bde4fec33cd479d36b27b729533f59e45ed4aaf1
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-10-04 12:39:28 +0700

    Add client entry points for ShortenForm and RedirectPage

---
+++ src/client/main.tsx
@@ -0,0 +1,7 @@
+import { render } from 'hono/jsx/dom'
+import ShortenForm from '../components/ShortenForm'
+
+const container = document.getElementById('shorten-form-container')
+if (container) {
+  render(<ShortenForm />, container)
+}
